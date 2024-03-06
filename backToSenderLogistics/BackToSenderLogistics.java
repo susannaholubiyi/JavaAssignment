8c13c505eb557ba0d10a3bb25824df01a14ddd5c
susannaholubiyi <susannaholubiyi@gmail.com>
2024-03-07 00:49:20 +0100

    added back to sender logistics

---
+++ backToSenderLogistics/BackToSenderLogistics.java
@@ -0,0 +1,33 @@
+package backToSender;
+public class BackToSenderLogistics {
+    int amountPerParcel;
+    int wage;
+    public int paymentCalculator(Integer numberOfDeliveries) {
+        validateNumberOfDeliveries(numberOfDeliveries);
+        try {
+            int basePay = 5000;
+            wage = (getAmountPerParcel(numberOfDeliveries) * numberOfDeliveries) + basePay;
+            return wage;
+        }
+        catch (Exception ignored){}
+        return 0;
+    }
+
+    private static void validateNumberOfDeliveries(Integer numberOfDeliveries) {
+        if (numberOfDeliveries < 0 || numberOfDeliveries >100){
+            throw new IllegalArgumentException("Invalid input");
+        }
+    }
+
+    private int getAmountPerParcel(Integer numberOfDeliveries) {
+        if (numberOfDeliveries < 50) {
+            return amountPerParcel = 160;
+        } else if (numberOfDeliveries < 60) {
+            return amountPerParcel = 200;
+        } else if (numberOfDeliveries < 70) {
+            return amountPerParcel = 250;
+        } else {
+            return amountPerParcel = 500;
+        }
+    }
+}
